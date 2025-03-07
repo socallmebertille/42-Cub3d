698e60b2368f2b223aefd4be20686167b42d3d1d
melinaaam <mmo.melina@gmail.com>
2025-03-07 11:57:22 +0100

    map bonus, img et debut parsing porte

--- srcs_bonus/map_parse_bonus.c
+++ srcs_bonus/map_parse_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_parse_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bertille <bertille@student.42.fr>          +#+  +:+       +#+        */
+/*   By: memotyle <memotyle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:23:14 by saberton          #+#    #+#             */
-/*   Updated: 2025/03/05 17:09:26 by bertille         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:55:08 by memotyle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static void	malloc_map(char **file, t_game *game)
 	len = 0;
 	while (file[i + len])
 	{
+		if (len > MAX_MAP_HEIGHT || (int)ft_strlen(file[i + len]) > MAX_MAP_WIDTH)
+			return (write_err(RED "Error\nMap too tall (max 100)\n" RESET));
 		if ((int)ft_strlen(file[i + len]) == ft_count_chars(file[i + len],
 				"\n\0\t\r\f\v "))
 			break ;
@@ -101,9 +103,9 @@ int	check_char(char **map, t_game *game)
 	while (map[++i])
 	{
 		recup_player(map[i], game, i);
-		if (!ft_find_others(map[i], "01 NSEW"))
+		if (!ft_find_others(map[i], "01 NSEWD"))
 			return (write_err(RED WRONG_CHAR RESET), 1);
-		count += ft_count_chars(map[i], "NSEW");
+		count += ft_count_chars(map[i], "NSEW");//D?
 	}
 	if (count < 1)
 		return (write_err(RED "Error\nA player is missing in map\n" RESET), 1);
